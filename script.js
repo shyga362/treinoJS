3bbd88d6dce3ff30cfa1d41d5e11244f0d30c93c
BrunoChiga <79880178+shyga362@users.noreply.github.com>
2022-10-02 12:41:33 -0300

    .

---
+++ script.js
@@ -0,0 +1,9 @@
+// vetores e array
+
+let array = ['string', 1, true, ['array1'], ['array2']];
+//console.log(array[]);
+
+//array.forEach(function(item, index){console.log(item, index)});
+
+array.push(1);
+console.log(array);
